3fb950d8df1db7dbb3a36486ea6592afc1756b09
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-08-04 08:19:21 +0200

    :wastebin: Deleted stray c-file

--- api.h
+++
@@ -1,9 +0,0 @@
-#ifndef _API_H_
-#define _API_H_
-
-#include <Rinternals.h>
-
-SEXP c_average_price(SEXP const x);
-SEXP c_bollinger_bands(const SEXP x, const SEXP timePeriod, const SEXP nbDevUp, const SEXP nbDevDn, SEXP maType);
-
-#endif
