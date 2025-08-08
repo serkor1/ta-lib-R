41811bf88fcafa227c4001d135bff393b423aeea
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-08-08 09:29:38 +0200

    :hammer: Unit-tests
    * Took the first steps towards proper unit-testing

---
+++ tests/testthat/test-ta_ACCBANDS.R
@@ -0,0 +1,17 @@
+## script: ACCBANDS
+## author: Serkan Korkmaz
+testthat::test_that(desc = "Acceleration Bands", code = {
+
+  ## 1) calculate values
+  ##    with and without alias
+  output <- acceleration_bands(SPY)
+  alias  <- ACCBANDS(SPY)
+
+  ## 1.1) check if the values
+  ##      are equal
+  testthat::expect_equal(
+    object    = output,
+    expected  = alias
+  )
+  
+})
