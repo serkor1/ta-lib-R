03c36ec830e80ed9e6313d29d2b228646b7bf9e2
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-08-08 07:28:19 +0200

    :hammer: Added options
    * The candlesticks (Only one, currently) have a an option to normalize the output.

--- R/ta_CDLDOJISTAR.R
+++ R/ta_CDLDOJISTAR.R
@@ -16,6 +16,7 @@ doji_star.default <- function(x, ...) {
         .open(x),
         .high(x),
         .low(x),
-        .close(x)
+        .close(x),
+        as.logical(getOption("talib.normalize", TRUE))
     )
 }
