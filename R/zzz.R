178c4850bbf4b13d9b7225ff9e7066f0707121c9
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-08-13 08:57:48 +0200

    :hammer: Added 'zzz.R'
    * The primary role of this is initialize the plotting environment.

---
+++ R/zzz.R
@@ -0,0 +1,25 @@
+# script: zzz
+# date: 2025-08-13
+# author: Serkan Korkmaz, serkor1@duck.com
+# objective:
+# script start;
+
+.plotting_environment <- new.env(
+  parent = emptyenv()
+)
+
+.onAttach <- function(
+    libname,
+    pkgname,
+    ...) {
+
+}
+
+.onDetach <- function(
+    libpath,
+    ...) {
+  
+
+}
+
+# script end;
