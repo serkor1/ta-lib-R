90ede8b92035be54edb3b3043ca7f574dde7620f
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-08-27 19:46:13 +0200

    :hammer: Ported ACCBANDS to new interface

--- R/ta_ACCBANDS.R
+++ R/ta_ACCBANDS.R
@@ -3,41 +3,73 @@
 #' @family Overlap Study
 #'
 #' @export
-acceleration_bands <- function(x, n = 10, ...) {
+acceleration_bands <- function(
+    x,
+    n = 10,
+    cols,
+    ...
+) {
     UseMethod("acceleration_bands")
 }
 
+#' @aliases acceleration_bands
 #' @export
 ACCBANDS <- acceleration_bands
 
 #' @export
-acceleration_bands.default <- function(x, n = 10, ...) {
-    ## default behaviour is to
-    ## coerce to a `matrix` check that
-    ## it is double and then pass to
-    ## C-side.
-
-    ## 0) validate input
-    ##    and stop the script
-    ##    if conditions are not
-    ##    met
-    if (!is.matrix(x)) {
-        x <- as.matrix(x)
+acceleration_bands.default <- function(
+    x,
+    cols,
+    n = 10,
+    ...
+) {
+    ## check input
+    ## cols if passed
+    if (!missing(cols)) {
+        assert(
+            is.formula(cols),
+            paste0(
+                "'cols' has to be <",
+                class(~s),
+                ">. ",
+                "Got <",
+                class(cols),
+                ">."
+            )
+        )
+        assert(
+            length(all.vars(cols)) == 3,
+            paste0(
+                "'cols' has to be length 3. ",
+                "Got length ",
+                length(all.vars(cols))
+            )
+        )
     }
-    assert(is.numeric(x))
+
+    HLC <- series(
+        formula = cols,
+        formula_default = ~ open + high + close,
+        data = x,
+        ...
+    )
+
     assert(n >= 2)
 
     ## 1) pass `x` assuming that it
     ##    follows OHLC-V structure
-    .Call(
-        "impl_ta_ACCBANDS",
-        .high(x),
-        .low(x),
-        .close(x),
-        as.integer(n)
+    as.data.frame(
+        .Call(
+            "impl_ta_ACCBANDS",
+            HLC[[1]],
+            HLC[[2]],
+            HLC[[3]],
+            as.integer(n)
+        )
     )
 }
 
+#' @aliases acceleration_bands
 #' @export
 acceleration_bands.plotly <- function(
     x,
@@ -59,32 +91,35 @@ acceleration_bands.plotly <- function(
     )
 
     ## calculate acceleration
-    ## bands
-    .indicator <- .Call(
-        "impl_ta_ACCBANDS",
-        .high(HLC),
-        .low(HLC),
-        .close(HLC),
-        as.integer(n)
+    ## bands and return as
+    ## data.frame
+    .indicator <- as.data.frame(
+        .Call(
+            "impl_ta_ACCBANDS",
+            HLC[[1]],
+            HLC[[2]],
+            HLC[[3]],
+            as.integer(n)
+        )
     )
 
-    ## convert to data.frame
-    data_frame <- data.frame(
-        idx = seq_along(.indicator),
-    )
+    .indicator$idx <- 1:nrow(.indicator)
 
-    ## generate plot
-    .chart <- .plotting_environment$main
-
+    ## add upper, middle and lower bands
+    ## to the main chart - wrapped in local
+    ## to circumvent lazy evaluation.
+    ##
+    ## NOTE: Otherwise it will only evaluate
+    ##       and add the last element
     for (i in seq_len(ncol(HLC))) {
         local({
             j <- i
 
             .plotting_environment$main <- plotly::add_lines(
-                .chart,
-                data = data_frame,
+                .plotting_environment$main,
+                data = .indicator,
                 x = ~idx,
-                y = ~ .value[, j],
+                y = ~ .indicator[, j],
                 inherit = FALSE,
                 line = list(
                     color = '#4682b4'
@@ -99,9 +134,10 @@ acceleration_bands.plotly <- function(
     .plotting_environment$main <- plotly::add_ribbons(
         p = .plotting_environment$main,
         inherit = FALSE,
-        x = ~ seq_len(nrow(.value)),
-        ymin = ~ .value[, 3],
-        ymax = ~ .value[, 1],
+        data = .indicator,
+        x = ~idx,
+        ymin = ~ .indicator[, 3],
+        ymax = ~ .indicator[, 1],
         fillcolor = plotly::toRGB("#4682b4", alpha = 0.2),
         line = list(
             color = "transparent"
@@ -109,7 +145,7 @@ acceleration_bands.plotly <- function(
         showlegend = TRUE,
         legendgroup = 'acceleration_band',
         name = paste0(
-            "ACCBANDS"
+            "Acceleration Bands"
         )
     )
 
