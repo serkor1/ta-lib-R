418ed4dce1e364e4314f80b2964e48c2af07ed80
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-08-28 10:39:43 +0200

    :hammer: Added some lose comments

--- R/chart_indicator.R
+++ R/chart_indicator.R
@@ -4,8 +4,18 @@ indicator <- function(
   cols,
   ...
 ) {
+  ## indicator
+  ##
+  ## description:
+  ## the indicator function builds upon existing
+  ## {plotly} objects if it has been defined, otherwise
+  ## it will create a standalone chart with the indicator
+  ## only
   parent_frame <- parent.frame()
 
+  ## resolve passed formula
+  ## via 'cols' and store in the
+  ## parent_frame
   has_var <- !missing(cols)
   if (has_var) {
     var_expr <- eval.parent(substitute(cols))
@@ -13,31 +23,29 @@ indicator <- function(
     environment(var_expr) <- parent_frame
   }
 
+  ## resolve the call
+  ## passed via FUN
   f_call <- substitute(FUN)
   pre_args <- list()
-  if (!is.call(f_call)) {
-    f <- match.fun(eval.parent(substitute(FUN)))
-  } else {
-    head <- f_call[[1L]]
-    if (is.symbol(head)) {
-      f <- get(as.character(head), envir = parent_frame, mode = "function")
-    } else if (is.call(head) && as.character(head[[1L]]) %in% c("::", ":::")) {
-      pkg <- as.character(head[[2L]])
-      fun <- as.character(head[[3L]])
-      f <- if (as.character(head[[1L]]) == "::") {
-        getExportedValue(pkg, fun)
-      } else {
-        get(fun, envir = asNamespace(pkg), mode = "function")
-      }
-    } else {
-      f <- eval(head, envir = parent_frame)
-    }
+  if (is.call(f_call)) {
+    f <- eval(f_call[[1L]], envir = parent_frame)
     pre_args <- as.list(f_call)[-1L]
+  } else {
+    f <- match.fun(eval.parent(f_call))
   }
 
-  # Ensure a base plot for .plotly methods
+  ## resolve plotting
+  ## environment; if it exists
+  ## it means talib::chart has been called
+  ## otherwise generate a new environment and
+  ## work off of that
   .plotting_environment <- get0(".plotting_environment", inherits = TRUE)
-  if (is.null(.plotting_environment) || is.null(.plotting_environment$main)) {
+  if (is.null(.plotting_environment)) {
+    .plotting_environment <- new.env(parent = emptyenv())
+    assign(".plotting_environment", .plotting_environment, inherits = TRUE)
+  }
+
+  if (is.null(.plotting_environment$main)) {
     .plotting_environment$main <- .chart_layout(
       x = plotly::plotly_empty(),
       title_text = "fisk"
@@ -46,37 +54,25 @@ indicator <- function(
 
   .plot <- .plotting_environment$main
 
-  # Build call
+  ## build the FUN call
+  ## with argument and let the
+  ## the dispatcher handle the rest
   dots <- as.list(substitute(list(...)))[-1L]
   args <- c(list(.plot), pre_args)
 
-  # Only add the formula arg if provided
   if (has_var) {
-    f_formals <- try(names(formals(f)), silent = TRUE)
-    if (inherits(f_formals, "try-error")) {
-      f_formals <- character()
-    }
-    formula_arg <- if ("cols" %in% f_formals) {
-      "cols"
-    } else if ("formula" %in% f_formals) {
-      "formula"
-    } else if ("cols" %in% f_formals) {
-      "cols"
-    } else {
-      "cols"
-    }
-    args[[formula_arg]] <- var_expr
+    f_formals <- tryCatch(names(formals(f)), error = function(e) character())
+    name <- intersect(c("cols", "formula"), f_formals)[1]
+    if (!is.na(name)) args[[name]] <- var_expr
   }
 
-  out <- do.call(f, c(args, dots), envir = parent_frame)
+  output <- do.call(f, c(args, dots), envir = parent_frame)
 
-  if (inherits(out, "plotly")) {
+  ## construct plotly object
+  ## if based off of the char
+  if (inherits(output, "plotly")) {
     panels <- c(list(.plotting_environment$main), .plotting_environment$sub)
-    stopifnot(all(vapply(
-      panels,
-      function(p) inherits(p, "plotly"),
-      logical(1)
-    )))
+    stopifnot(all(vapply(panels, inherits, logical(1), what = "plotly")))
     n <- length(panels)
     main_h <- getOption("talib.chart.main", 0.7)
     heights <- if (n > 1) c(main_h, rep((1 - main_h) / (n - 1), n - 1)) else 1
@@ -91,5 +87,5 @@ indicator <- function(
     return(fig)
   }
 
-  out
+  output
 }
