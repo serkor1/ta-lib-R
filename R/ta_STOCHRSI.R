0a65d05dfbce3d1026ceb3db2396d242c9b12d42
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-08-31 09:28:57 +0200

    :hammer: Implemented RSI and STOCHRSI
    * This revealed some bugs. indicator with custom data does not
        work

--- R/ta_STOCHRSI.R
+++ R/ta_STOCHRSI.R
@@ -5,7 +5,7 @@
 #'
 #' @templateVar .title Stochastic Relative Strength Index
 #' @templateVar .author Serkan Korkmaz
-#' @templateVar .fun acceleration_bands
+#' @templateVar .fun stochastic_relative_strength_index
 #'
 #' @template description
 stochastic_relative_strength_index <- function(
@@ -27,19 +27,139 @@ STOCHRSI <- stochastic_relative_strength_index
 #' @export
 stochastic_relative_strength_index.default <- function(
 	x,
-	n,
-	fast_k,
+	cols,
+	n = 10,
+	fast_k = 5,
 	fast_d_MAtype = SMA(n = 10),
 	...
 ) {
 	fast_d_MAtype <- map_maType_call(substitute(fast_d_MAtype))
 
-	.Call(
-		"impl_ta_STOCHRSI",
-		x,
-		as.integer(n),
-		as.integer(fast_k),
-		fast_d_MAtype$n,
-		fast_d_MAtype$maType
+	## check input
+	## cols if passed
+	if (!missing(cols)) {
+		assert(
+			is.formula(cols),
+			paste0(
+				"'cols' has to be <",
+				class(~s),
+				">. ",
+				"Got <",
+				class(cols),
+				">."
+			)
+		)
+		assert(
+			length(all.vars(cols)) == 1,
+			paste0(
+				"'cols' has to be length 1. ",
+				"Got length ",
+				length(all.vars(cols))
+			)
+		)
+	}
+
+	x <- series(
+		x = cols,
+		default = ~RSI,
+		data = x,
+		...
 	)
+
+	as.data.frame(
+		.Call(
+			"impl_ta_STOCHRSI",
+			x[[1]],
+			as.integer(n),
+			as.integer(fast_k),
+			fast_d_MAtype$n,
+			fast_d_MAtype$maType
+		)
+	)
+}
+
+
+#' @usage NULL
+#' @aliases stochastic_relative_strength_index
+#' @export
+stochastic_relative_strength_index.data.frame <- function(
+	x,
+	cols,
+	n = 10,
+	fast_k = 5,
+	fast_d_MAtype = SMA(n = 10),
+	...
+) {
+	as.data.frame(
+		NextMethod()
+	)
+}
+
+#' @usage NULL
+#' @aliases stochastic_relative_strength_index
+#' @export
+stochastic_relative_strength_index.matrix <- function(
+	x,
+	cols,
+	n = 10,
+	fast_k = 5,
+	fast_d_MAtype = SMA(n = 10),
+	...
+) {
+	as.matrix(
+		NextMethod()
+	)
+}
+
+#' @usage NULL
+#' @aliases stochastic_relative_strength_index
+#' @export
+stochastic_relative_strength_index.plotly <- function(
+	x,
+	cols,
+	n = 10,
+	fast_k = 5,
+	fast_d_MAtype = SMA(n = 10),
+	data,
+	...
+) {
+	## prepare series
+	## from
+	x <- as.data.frame(
+		series(
+			x = x,
+			formula = cols,
+			default = ~open,
+			data = data,
+			...
+		)
+	)
+
+	## indicator
+	.indicator <- as.data.frame(
+		NextMethod()
+	)
+	.indicator$idx <- 1:nrow(.indicator)
+
+	rsi_plot <- plotly::plot_ly(
+		.indicator,
+		x = ~idx,
+		y = ~fastk,
+		type = "scatter",
+		mode = "lines",
+		showlegend = FALSE
+	)
+	rsi_plot <- plotly::add_lines(
+		.indicator,
+		x = ~idx,
+		y = ~fastk,
+		showlegend = FALSE
+	)
+
+	.plotting_environment$sub <- c(
+		.plotting_environment$sub,
+		list(rsi_plot)
+	)
+
+	rsi_plot
 }
---
+++ tests/testthat/test-ta_STOCHRSI.R
@@ -0,0 +1,55 @@
+## script: Stochastic Relative Strength Index
+## author: Serkan Korkmaz
+testthat::test_that(desc = "Stochastic Relative Strength Index", code = {
+	## 1) calculate values
+	##    with and without alias
+	output <- stochastic_relative_strength_index(RSI(SPY))
+	alias <- STOCHRSI(RSI(SPY))
+
+	## 1.1) check if the values
+	##      are equal
+	testthat::expect_equal(
+		object = output,
+		expected = alias
+	)
+
+	## 2) test that charting
+	##    works
+	output <- testthat::expect_no_error(
+		{
+			chart(BTC)
+			indicator(stochastic_relative_strength_index(), data = RSI(BTC))
+		}
+	)
+
+	## 2.1) test that it outputs
+	##      a plotly object
+	testthat::expect_true(
+		inherits(output, "plotly")
+	)
+
+	## 3) test class-in and class-out
+	##    equality
+
+	## 3.1) matrix
+	testthat::expect_true(
+		inherits(stochastic_relative_strength_index(RSI(SPY)), class(RSI(SPY)))
+	)
+
+	## 3.2) data.frame
+	testthat::expect_true(
+		inherits(stochastic_relative_strength_index(RSI(BTC)), class(RSI(BTC)))
+	)
+
+	## 4) test that default
+	##    values equals
+	testthat::expect_equal(
+		object = stochastic_relative_strength_index(
+			RSI(BTC)
+		),
+		expected = stochastic_relative_strength_index(
+			RSI(BTC),
+			cols = ~RSI
+		)
+	)
+})
