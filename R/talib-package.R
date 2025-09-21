ea9451cdb5c4291118cd50a36eb2bfec076664ba
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-09-21 10:47:34 +0200

    :books: Added generic parameter description
    * The paramters are added to generic roxygen blocks as inherit
        this avoids enforcing documentation on paramters that doesnt exist.
        It will only inherit parameters that exists.

--- R/talib-package.R
+++ R/talib-package.R
@@ -2,3 +2,21 @@
 #' @useDynLib talib, .registration = TRUE
 ## usethis namespace: end
 NULL
+
+#' @title Generic function documentation
+#' @name generic_documentation
+#'
+#' @description
+#' A generic documentation block for documenting parameters that
+#' are common across all functions. Avoids documenting parameters
+#' that doesn't exist downstream.
+#'
+#' @param x An OHLC-V series that is coercible to [data.frame]. The function assumes that all columns are named in lowercase and order invariant.
+#' @param cols An optional [formula] passed into [model.frame]. If passed into indicators based on univariate series, the function calculates indicators for each element in 'cols'. For indicators based on multivariate series, it will alter the calculation itself. See `vignette("talib")` for more details.
+#' @param n An [integer] of [length] 1.
+#' @param ... Additional parameters passed into [model.frame]
+#'
+#' @returns NULL
+#' @keywords internal
+#' @usage NULL
+NULL
