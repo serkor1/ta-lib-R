0e55ad1aae42ca0a1d74b20af3ae50fcf5eb94f4
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-09-25 22:13:16 +0200

    :hammer: Hilbert Transform Tests

---
+++ tests/testthat/test-ta_HT_PHASOR.R
@@ -0,0 +1,55 @@
+## script: HTPHASOR
+## author: Serkan Korkmaz
+testthat::test_that(desc = "HT PHASOR", code = {
+	## 1) calculate values
+	##    with and without alias
+	output <- ht_phasor(SPY)
+	alias <- HT_PHASOR(SPY)
+
+	## 1.1) check if the values
+	##      are equal
+	testthat::expect_equal(
+		object = output,
+		expected = alias
+	)
+
+	## 2) test that charting
+	##    works
+	output <- testthat::expect_no_error(
+		{
+			chart(BTC)
+			indicator(ht_phasor)
+		}
+	)
+
+	## 2.1) test that it outputs
+	##      a plotly object
+	testthat::expect_true(
+		inherits(output, "plotly")
+	)
+
+	## 3) test class-in and class-out
+	##    equality
+
+	## 3.1) matrix
+	testthat::expect_true(
+		inherits(ht_phasor(SPY), class(SPY))
+	)
+
+	## 3.2) data.frame
+	testthat::expect_true(
+		inherits(ht_phasor(BTC), class(BTC))
+	)
+
+	## 4) test that default
+	##    values equals
+	testthat::expect_equal(
+		object = ht_phasor(
+			BTC
+		),
+		expected = ht_phasor(
+			BTC,
+			cols = ~open
+		)
+	)
+})
