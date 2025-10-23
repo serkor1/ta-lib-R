1a9c76cbffe7130b3f2bf36777f6923b0d0462c7
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-10-23 18:55:05 +0200

    :fire: Last batch of 'Momentum Indicator'
    * I will skip the scaled versions of ROC and ROCR. Its such a trivial addition that
      it makes no sense to add. To achieve the same thing in C, you'd need to write
      a full for loop, or a branched loop. So it makes sense there, not here.
    
      The real challenge is whether my OCD can handle it, lol.

---
+++ R/ta_ROCR.R
@@ -0,0 +1,175 @@
+#' @export
+#' @family Momentum Indicator
+#'
+#' @title Ratio of Change
+#' @templateVar .title Ratio of Change
+#' @templateVar .author Serkan Korkmaz
+#' @templateVar .fun ratio_of_change
+#'
+## splice:documentation:start
+## splice:documentation:end
+#'
+#' @template description
+ratio_of_change <- function(
+	x,
+	cols,
+	n = 10,
+	...
+) {
+	UseMethod("ratio_of_change")
+}
+
+#' @export
+#' @usage NULL
+#' @rdname ratio_of_change
+#'
+#' @aliases ratio_of_change
+ROCR <- ratio_of_change
+
+#' @usage NULL
+#' @aliases ratio_of_change
+#'
+#' @export
+ratio_of_change.default <- function(
+	x,
+	cols,
+	n = 10,
+	...
+) {
+	## validate 'cols'-argument
+	## if explicitly passed
+	if (!missing(cols)) {
+		assert_formula(cols)
+	}
+
+	## extract rownames
+	## for later attachment
+	x_names <- rownames(x)
+
+	## construct series
+	## from input
+	constructed_series <- series(
+		x = cols,
+		default = ~close,
+		data = x,
+		...
+	)
+
+	## calculate indicator and
+	## return as data.frame
+	x <- .Call(
+		"impl_ta_ROCR",
+		## splice:call:start
+		constructed_series[[1]],
+		as.integer(n)
+		## splice:call:end
+	)
+
+	## readd rownames
+	rownames(x) <- x_names
+
+	## return indicator
+	x
+}
+
+#' @usage NULL
+#' @aliases ratio_of_change
+#'
+#' @export
+ratio_of_change.data.frame <- function(
+	x,
+	cols,
+	n = 10,
+	...
+) {
+	as.data.frame(
+		NextMethod()
+	)
+}
+
+#' @usage NULL
+#' @aliases ratio_of_change
+#'
+#' @export
+ratio_of_change.matrix <- function(
+	x,
+	cols,
+	n = 10,
+	...
+) {
+	as.matrix(
+		NextMethod()
+	)
+}
+
+#' @usage NULL
+#' @aliases ratio_of_change
+#'
+#' @export
+ratio_of_change.plotly <- function(
+	x,
+	cols,
+	n = 10,
+	## splice:optional-plotly:start
+	## splice:optional-plotly:end
+	...
+) {
+	## check that input value
+	## 'x' is <plotly>-object
+	assert_plotly(x)
+
+	## check that input value
+	## 'cols' is a <formula>-objet
+	if (!missing(cols)) {
+		assert_formula(cols)
+	}
+
+	## construct series from
+	## {plotly}-object
+	constructed_series <- series(
+		x = x,
+		formula = cols,
+		default = ~close,
+		...
+	)
+
+	## construct indicator
+	## from the series
+	constructed_indicator <- ratio_of_change(
+		x = constructed_series,
+		cols = rebuild_formula(
+			names(constructed_series)
+		),
+		n = n
+	)
+
+	## add conditional idx
+	constructed_indicator[["idx"]] <- add_idx(
+		constructed_series
+	)
+
+	## construct {plotly}-object
+	## splice:plotly-assembly:start
+	plotly_object <- subchart(
+		data = constructed_indicator,
+		y = ~ROCR,
+		type = "scatter",
+		mode = "lines",
+		showlegend = FALSE
+	)
+
+	if (main_chart_exists()) {
+		plotly_object <- add_title(
+			x = plotly_object,
+			text = "Ratio of Change"
+		)
+	}
+
+	.plotting_environment$sub <- c(
+		.plotting_environment$sub,
+		list(plotly_object)
+	)
+	## splice:plotly-assembly:end
+
+	plotly_object
+}
