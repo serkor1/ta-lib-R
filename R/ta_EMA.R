9672efb7cd00e9cbd0cbf467df29565ab1f40cb0
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-10-23 21:59:28 +0200

    :hammer: Updated MA methods
    * See 58016a947648f8223ec27fe2cf8641e8cffdee0c if necessary for more details.
      A proper MA method should apply to a <vector>, without that aspect its just too
      complicated.

--- R/ta_EMA.R
+++ R/ta_EMA.R
@@ -116,9 +116,49 @@ exponential_moving_average.matrix <- function(
 	n = 10,
 	...
 ) {
+	## pass directly to
+	## exponential_moving_average.default to avoid
+	## shenanigans with NextMethod()
 	as.matrix(
-		NextMethod()
+		exponential_moving_average.default(
+			x = x,
+			cols = cols,
+			n = n,
+			...
+		)
 	)
+}
+
+#' @usage NULL
+#' @aliases exponential_moving_average
+#'
+#' @export
+exponential_moving_average.numeric <- function(
+	x,
+	cols,
+	n = 10,
+	...
+) {
+	## warn if 'cols' have been
+	## passed just to make sure
+	## the user knows its not possible
+	## or relevant
+	if (!missing(cols)) {
+		warning("'cols' is passed but is unused for vectors.")
+	}
+
+	## pass to 'C' directly
+	## with the input vector
+	x <- .Call(
+		"impl_ta_MA",
+		as.double(x),
+		as.integer(n),
+		1L
+	)
+
+	## 'C' returns a named matrix
+	## return the first column
+	as.double(x)
 }
 
 #' @usage NULL
