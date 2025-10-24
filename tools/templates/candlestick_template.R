2f2381acedf106e9680ae9c9929dc583f01fea0b
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-10-24 07:34:45 +0200

    :hammer: Candlestick template
    * The template used for 770bc3acc2a70a719e7e4112c5a77fe71da1cf12

---
+++ tools/templates/candlestick_template.R
@@ -0,0 +1,163 @@
+#' @export
+#' @family Pattern Recognition
+#'
+#' @title $TITLE
+#' @templateVar .title $TITLE
+#' @templateVar .author Serkan Korkmaz
+#' @templateVar .fun $FUN
+#' 
+## splice:documentation:start
+## splice:documentation:end
+#'
+#' @template description
+$FUN <- function(
+	x, 
+	cols, $SIG_FORMALS
+	...) {
+  UseMethod("$FUN")
+}
+
+#' @export
+#' @usage NULL
+#' @rdname $FUN
+#' 
+#' @aliases $FUN
+$ALIAS <- $FUN
+
+#' @usage NULL
+#' @aliases $FUN
+#' 
+#' @export
+$FUN.default <- function(
+	x, 
+	cols, $SIG_FORMALS 
+	...) {
+
+	## get normalization option
+	normalize <- as.logical(
+		getOption("talib.normalize", TRUE)
+	)
+
+	## validate 'cols'-argument 
+	## if explicitly passed
+	if (!missing(cols)) {
+		assert_formula(cols)
+	}
+
+	## construct series
+	## from input
+	constructed_series <- series(
+		x = cols,
+		default = ~open + high + low + close,
+		data = x,
+		...
+	)
+
+	## extract rownames
+	## for later attachment
+	x_names <- rownames(x)
+
+	## calculate indicator and
+	## return as data.frame
+	x <- as.matrix(
+		.Call(
+			"impl_ta_$ALIAS",
+		constructed_series[[1]],
+		constructed_series[[2]],
+		constructed_series[[3]],
+		constructed_series[[4]]
+		$SIG_ACTUALS
+		,normalize
+	)
+	)
+
+	## add column name
+	colnames(x) <- "$ALIAS"
+
+	## readd rownames
+	rownames(x) <- x_names
+
+	## return indicator
+	x
+}
+
+#' @usage NULL
+#' @aliases $FUN
+#' 
+#' @export
+$FUN.data.frame <- function(
+	x, 
+	cols, $SIG_FORMALS 
+	...) {
+
+	as.data.frame(
+		NextMethod()
+	) 
+}
+
+#' @usage NULL
+#' @aliases $FUN
+#' 
+#' @export
+$FUN.matrix <- function(
+	x, 
+	cols,$SIG_FORMALS 
+	...) { 
+		as.matrix(
+			NextMethod()
+			) 
+			}
+
+#' @usage NULL
+#' @aliases $FUN
+#' 
+#' @export
+$FUN.plotly <- function(
+	x, 
+	cols, $SIG_FORMALS
+	...) { 
+
+	## check that input value
+	## 'x' is <plotly>-object
+	assert_plotly(x)
+
+	## check that input value
+	## 'cols' is a <formula>-objet
+	if (!missing(cols)) {
+		assert_formula(cols)
+	}
+
+	## construct series from 
+	## {plotly}-object
+	constructed_series <- series(
+		x = x,
+		formula = cols,
+		default = ~open + high + low + close,
+		...
+	)
+
+	## construct indicator
+	## from the series
+	constructed_indicator <- $FUN(
+		x = constructed_series,
+		cols = rebuild_formula(
+			names(constructed_series)
+		)$SIG_ACTUALS
+	)
+
+	## add conditional idx
+	constructed_indicator[["idx"]] <- add_idx(
+		constructed_series
+	)
+
+	## construct {plotly}-object
+	plotly_object <- .plotting_environment[["main"]] <-  pattern(
+		p = .plotting_environment[["main"]],
+		x = constructed_indicator,
+		high = constructed_series[[2]],
+		low = constructed_series[[3]],
+		pattern_name = "$FUN",
+		agnostic = $DEFAULT_FORMULA	)
+
+	plotly_object
+ }
