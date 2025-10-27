b93da099486aa94b9ce5c7d457cd4bf25f7b7250
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-10-27 15:09:24 +0100

    :hammer: Include pkg load/unload tests

---
+++ tests/testthat/test-pkg.R
@@ -0,0 +1,17 @@
+## 1) check that TA-Libs
+##    unloads
+testthat::test_that(desc = "TA-Lib unloads without error", code = {
+	## 1) unload without error
+	testthat::expect_no_condition(
+		detach("package:talib")
+	)
+})
+
+## 2) check that TA-Lib
+##    loads (NOTE: The order matters here because the package is already loaded when the tests are run, and gets unloaded above)
+testthat::test_that(desc = "TA-Lib loads without error", code = {
+	## 1) load without error
+	testthat::expect_no_condition(
+		library("talib")
+	)
+})
