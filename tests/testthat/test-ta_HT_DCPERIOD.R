9249f12d783c2e1b707436f0386b77777805486a
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-11-11 19:53:11 +0100

    :hammer: Re-rendered tests (24fad99f927c857d5566cf2b379e3288dead3932)

--- tests/testthat/test-ta_HT_DCPERIOD.R
+++ tests/testthat/test-ta_HT_DCPERIOD.R
@@ -122,3 +122,11 @@ testthat::test_that(desc = '{plotly}-methods for <matrix>', code = {
 		inherits(output, "plotly")
 	)
 })
+
+testthat::test_that(desc = 'Numeric Methods', code = {
+	## 1) check that the output class
+	##    matches the input class
+	testthat::expect_no_error(
+		dominant_cycle_period(BTC[[1]])
+	)
+})
