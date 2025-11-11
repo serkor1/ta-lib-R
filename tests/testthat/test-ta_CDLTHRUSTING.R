b65618b366f42acfd7834be6c350db9a2f4ff5ba
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-11-11 21:53:29 +0100

    :hammer: Similar to ad4be780bd9c5e27a5dd4fb9bca16d024678400a
    * unit-tests for <numeric> methods have been expanded for
      all indicators.
      - NOTE: There is no testing for <double> here because some functions
              returns <matrix> with <integer> (Candles for example.)

--- tests/testthat/test-ta_CDLTHRUSTING.R
+++ tests/testthat/test-ta_CDLTHRUSTING.R
@@ -4,9 +4,9 @@
 ##
 ## author: Serkan Korkmaz
 
-## 1) alias and function similarity
-##    checks this ensures that
-##    thrusting and CDLTHRUSTING produces the same results
+## alias and function similarity
+## checks this ensures that
+## thrusting and CDLTHRUSTING produces the same results
 testthat::test_that(desc = 'Alias and function similarity', code = {
 	## 1) test that the alias and
 	##    function returns the same values
@@ -21,10 +21,10 @@ testthat::test_that(desc = 'Alias and function similarity', code = {
 	)
 })
 
-## 3) type-checks for data.frames and
-##    matrices
+## type-checks for data.frames and
+## matrices
 ##
-## 3.1) <matrix> object
+## <matrix> object
 testthat::test_that(desc = 'Class in, class out (<matrix>)', code = {
 	## 1) check that the output class
 	##    matches the input class
@@ -33,7 +33,7 @@ testthat::test_that(desc = 'Class in, class out (<matrix>)', code = {
 	)
 })
 
-## 3.2) <data.frame> object
+## <data.frame> object
 testthat::test_that(desc = 'Class in, class out (<data.frame>)', code = {
 	## 1) check that the output class
 	##    matches the input class
@@ -42,12 +42,12 @@ testthat::test_that(desc = 'Class in, class out (<data.frame>)', code = {
 	)
 })
 
-## 4) check that the default calls
-##    matches that of the constructed call
-##    with default values.
+## check that the default calls
+## matches that of the constructed call
+## with default values.
 ##
-##    NOTE: This test is more of a test of the internal
-##          series() function
+## NOTE: This test is more of a test of the internal
+##       series() function
 testthat::test_that(desc = 'Default calls', code = {
 	testthat::expect_equal(
 		object = thrusting(
@@ -60,10 +60,10 @@ testthat::test_that(desc = 'Default calls', code = {
 	)
 })
 
-## 5) check that the lenght of the input
-##    matches the output length
+## check that the lenght of the input
+## matches the output length
 ##
-## 5.1) <data.frame> object
+## <data.frame> object
 testthat::test_that(desc = 'Equal length of input and output for <data.frame>', code = {
 	testthat::expect_equal(
 		object = nrow(thrusting(
@@ -73,7 +73,7 @@ testthat::test_that(desc = 'Equal length of input and output for <data.frame>', 
 	)
 })
 
-## 5.2) <matrix> object
+## <matrix> object
 testthat::test_that(desc = 'Equal length of input and output for <matrix>', code = {
 	testthat::expect_equal(
 		object = nrow(thrusting(
@@ -84,13 +84,13 @@ testthat::test_that(desc = 'Equal length of input and output for <matrix>', code
 })
 
 
-## 2) {plotly}-method checks for data.frames
-##    and matrices
+## <plotly>-method checks for <data.frame>
+## and <matrix>
 ##
-## 2.1) data.frame checks
-testthat::test_that(desc = '{plotly}-methods for <data.frame>', code = {
-	## 1) check that thrusting can
-	##    use {plotly} without any issues
+## <data.frame> checks
+testthat::test_that(desc = '<plotly>-methods for <data.frame>', code = {
+	## check that thrusting can
+	## use <plotly> without any issues
 	output <- testthat::expect_no_error(
 		{
 			chart(BTC)
@@ -98,17 +98,17 @@ testthat::test_that(desc = '{plotly}-methods for <data.frame>', code = {
 		}
 	)
 
-	## 1.1) check that the output
-	##      is a {plotly}-object
+	## check that the output
+	## is a <plotly>-object
 	testthat::expect_true(
 		inherits(output, "plotly")
 	)
 })
 
-## 2.2) matrix checks
-testthat::test_that(desc = '{plotly}-methods for <matrix>', code = {
-	## 1) check that thrusting can
-	##    use {plotly} without any issues
+## <matrix> checks
+testthat::test_that(desc = '<plotly>-methods for <matrix>', code = {
+	## check that thrusting can
+	## use <plotly> without any issues
 	output <- testthat::expect_no_error(
 		{
 			chart(SPY)
@@ -116,8 +116,8 @@ testthat::test_that(desc = '{plotly}-methods for <matrix>', code = {
 		}
 	)
 
-	## 1.1) check that the output
-	##      is a {plotly}-object
+	## check that the output
+	## is a <plotly>-object
 	testthat::expect_true(
 		inherits(output, "plotly")
 	)
