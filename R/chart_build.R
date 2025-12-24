e9bd9494f710ee79337578ac0b2817c46128689b
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-12-24 23:46:21 +0100

    :hammer: Updated build_plotly
    * The function is now internally documented
    * The function will now build the minimum required
      elements so the resulting <plotly>-object is uniform

--- R/chart_build.R
+++ R/chart_build.R
@@ -1,16 +1,19 @@
-#' @title Build Plot
+#' @title build_plotly
 #'
 #' @description
-#' Placeholder
+#' A high-level <plotly> builder for the <plotly>-methods.
 #'
-#' @param init placeholder
-#' @param traces placeholder
-#' @param name placeholder
+#' @param init A <plotly>-object to be built, or built upon.
+#' @param traces A nested <[list]> of <plotly> arguments.
+#' @param decorators A <[list]> of functions that decorates the <plotly>-objecty. Can be an empty <[list]>.
+#' @param name A <[character]>-vector of [length] 1. The name of the indicator; relevant mainly for univariate series.
+#' @param data A <[data.frame]> with the calculated indicator.
+#' @param title A <[character]>-vector of [length] 1. This adds a title to the subchart.
 #'
-#' @keywords internal
 #' @returns
 #' A <plotly>-object
 #'
+#' @keywords internal
 build_plotly <- function(
 	init,
 	traces,
@@ -33,9 +36,20 @@ build_plotly.plotly <- function(
 	title = NULL,
 	...
 ) {
-	# data fallback
+	## added to avoid
+	## having to go back and
+	## recode everything
 	if (missing(data)) {
-		data <- get("constructed_indicator", parent.frame())
+		data <- get(
+			"constructed_indicator",
+			parent.frame()
+		)
+	}
+
+	## replace missing names
+	## with title
+	if (missing(name) | is.null(name)) {
+		name <- title
 	}
 
 	# default for non-line traces
@@ -44,48 +58,56 @@ build_plotly.plotly <- function(
 		mode = "lines",
 		showlegend = TRUE,
 		inherit = FALSE,
+
+		## name and legendgroup
+		## has to be unique
 		name = name,
 		legendgroup = name,
-		legendgrouptitle = list(text = title),
+		legendgrouptitle = list(
+			text = if (missing(title)) {
+				name
+			} else {
+				title
+			}
+		),
 		data = data,
 		x = ~idx
 	)
 
 	# identify plotly_line traces
-	is_line <- vapply(traces, inherits, logical(1), "plotly_line")
+	is_line <- vapply(
+		traces,
+		inherits,
+		logical(1),
+		"plotly_line"
+	)
+
 	non_line <- which(!is_line)
 
 	# apply defaults only to non-line traces
 	if (length(non_line) > 0) {
 		traces[non_line] <- lapply(
 			traces[non_line],
-			function(tr) utils::modifyList(default_trace, tr)
+			function(tr) {
+				utils::modifyList(
+					default_trace,
+					tr,
+					keep.null = TRUE
+				)
+			}
 		)
 	}
 
 	n_tr <- length(traces)
 	n_non <- length(non_line)
 
-	# if (n_non > 0) {
-	# 	if (n_tr > 1 && n_non > 1) {
-	# 		# add a dedicated legend entry
-	# 		#legend_tr <- traces[[non_line[1]]]
-	# 		legend_tr <- list()
-	# 		legend_tr$showlegend <- TRUE
-	# 		legend_tr$visible <- "legendonly"
-	# 		legend_tr$name <- name
-
-	# 		traces <- append(traces, list(legend_tr))
-	# 	} else {
-	# 		# just name the single relevant non-line trace
-	# 		traces[[non_line[1]]]$name <- name
-	# 	}
-	# }
-
 	# build plotly object
 	plotly_object <- Reduce(
 		f = function(acc, tr) {
-			do.call(plotly::add_trace, c(list(acc), tr))
+			do.call(
+				plotly::add_trace,
+				c(list(acc), tr)
+			)
 		},
 		x = traces,
 		init = init
@@ -93,7 +115,10 @@ build_plotly.plotly <- function(
 
 	# decorate
 	if (!is.null(title)) {
-		plotly_object <- add_title(plotly_object, text = title)
+		plotly_object <- add_title(
+			plotly_object,
+			text = title
+		)
 	}
 
 	# check for additional options
@@ -105,5 +130,15 @@ build_plotly.plotly <- function(
 		)
 	}
 
-	layout_axis(plotly_object, data$idx)
+	## common decorators
+	fns <- list(
+		function(p) layout_background(p),
+		function(p) layout_axis(p, data$idx)
+	)
+
+	Reduce(
+		f = function(p, f) f(p),
+		x = fns,
+		init = plotly_object
+	)
 }
