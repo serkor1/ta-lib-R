9cc55ea85b0c35070d314f91e234e89d721509bd
serkor1 <77464572+serkor1@users.noreply.github.com>
2026-02-07 15:25:33 +0100

    :hammer: Refactored ta_VOLUME.c
    * The core implementation have been reworked and simplified
      completely. Most notably a bug has been fixed where if no
      maSpec were passed the program would crash on R-side because
      the function would return a vector.
    
    * The implementation uses TA-Libs TA_MA() instead of the created R API
      and the function is now significantly faster.

--- src/ta_VOLUME.c
+++ src/ta_VOLUME.c
@@ -1,153 +1,107 @@
-// Volume Indicator
+// ta_VOLUME.c
 //
 // Parameters
 //   double inReal
-//   list   maSpec
+//   list   maSpec  (each element is integer(2): c(period, maType))
 //
 // Returns
-//   matrix (n x k) where k is the number of
-//   maSpecs
+//   matrix (n x (1 + length(maSpec))) with columns:
+//     "VOLUME", "<MAType><period>" e.g. "SMA7"
 //
-// Details
-//   maSpec is a list of integers
-//
-// TODO: Simplify this program
 #include "MAType.h"
 #include "container.h"
 #include "lib.h"
 #include "names.h"
 #include "shift.h"
-#include "ta_MA.h"
 #include <R.h>
 #include <Rinternals.h>
+#include <stdio.h>
 #include <string.h>
 #include <ta_libc.h>
 
 // clang-format off
 SEXP impl_ta_VOLUME(
-    SEXP inReal,
-    SEXP maSpec
+  SEXP inReal, 
+  SEXP maSpec
 )
 // clang-format on
 {
-
-  // return 'inReal' if 'maSpec' is empty
-  // or NULL
-  if (isNull(maSpec) || LENGTH(maSpec) == 0) {
-    return inReal;
-  }
-
   // protection counter
   int protection_count = 0;
 
-  // get length of 'inReal' (assumes equal length across input)
+  // get length of 'inHigh' (assumes equal length across input)
   const int n = LENGTH(inReal);
+  const double *restrict x = REAL(inReal);
 
-  // pointers to input arrays
-  const double *restrict inReal_ptr = REAL(inReal);
-
-  // extract values passed downstream
-  // to impl_ta_MA
-  const int n_ma = LENGTH(maSpec);
-  const int n_cols = 1 + n_ma; // VOLUME + each MA
+  // determine maSpec input
+  const int n_ma = isNull(maSpec) ? 0 : LENGTH(maSpec);
+  const int n_cols = 1 + n_ma;
 
   // output
   SEXP output;
-  double *restrict output_ptr;
+  double *output_ptr;
 
-  // construct output container with
-  // lookback set to 0 (lookbacks are handled by impl_ta_MA)
-  const int proceed =
-    output_container(n, 0, n_cols, &output, &output_ptr, &protection_count);
+  // the output container is either a INTSXP or
+  // REALSXP depending on the type and will
+  // return a matrix with <NA> if there is a mismatch
+  // between lookback and n
+  //
+  // see container.h for more details
+  output_container(n, 0, n_cols, &output, &output_ptr, &protection_count);
+  memcpy(output_ptr, x, (size_t)n * sizeof(double));
 
-  // copy inReal to the output
-  // container
-  // clang-format off
-  memcpy(
-    output_ptr, 
-    inReal_ptr, 
-    (size_t) n * sizeof(double)
-  );
-  // clang-format on
-
-  // prepare column names for the output container
-  // (determined at runtime)
-  const char **colname = (const char **)R_alloc((size_t)n_cols, sizeof(char *));
-
-  // first column name is *always* VOLUME
+  // set initial column name
+  const char **colname =
+    (const char **)R_alloc((size_t)n_cols, sizeof(*colname));
   colname[0] = "VOLUME";
 
+  // iterate over maSpec
   for (int j = 0; j < n_ma; ++j) {
-    int protect_inner = 0;
+    int start_idx = 0;
+    int end_idx = 0;
 
-    // extract specifications
-    SEXP spec = VECTOR_ELT(maSpec, j);
+    // extract maSpec
+    //
+    // specification is a downstream enum
+    // period is passed in the downstream enum
+    SEXP specification = VECTOR_ELT(maSpec, j);
+    const int *specification_ptr = INTEGER(specification);
 
-    // pointers to specification
-    const int *spec_ptr = INTEGER(spec);
+    const int period = specification_ptr[0];
+    const TA_MAType ma_type = (TA_MAType)specification_ptr[1];
 
-    // values
-    const int optInTimePeriod = spec_ptr[0];
-    const int optInMAType = spec_ptr[1];
-
-    // impl_ta_MA expects SEXP
-    // clang-format off
-    SEXP period_sexp = PROTECT(
-      ScalarInteger(optInTimePeriod)
-    ); protect_inner++;
-    SEXP maType_sexp = PROTECT(
-      ScalarInteger(optInMAType)
-    ); protect_inner++;
-    // clang-format on
-
-    // calculate Moving Averages
-    // clang-format off
-    SEXP ma_res = PROTECT(
-      impl_ta_MA(
-        inReal, 
-        period_sexp, 
-        maType_sexp
-      )
-    ); protect_inner++;
-    // clang-format on
-
-    // pointer to results
-    double *ma_ptr = REAL(ma_res);
-
-    // copy to output
-    double *col_ptr = output_ptr + (size_t)(j + 1) * n;
+    double *restrict offset_real = output_ptr + (size_t)(j + 1) * (size_t)n;
 
     // clang-format off
-    memcpy(
-      col_ptr, 
-      ma_ptr, 
-      (size_t)n * sizeof(double)
+    // calculate moving averages
+    TA_RetCode return_value = TA_MA(
+      0,
+      n - 1,
+      x,
+      period, 
+      ma_type, 
+      &start_idx, 
+      &end_idx, 
+      offset_real
     );
+
+    // validate output
+    check_output(return_value, protection_count);
     // clang-format on
 
-    // construct column names
-    // as TA_MAType+optInTimePeriod
-    // ie. SMA17
-    // see see names.h for more details
-    const char *maTypeName = _MAType_((TA_MAType)optInMAType);
-    char *name_buffer = (char *)R_alloc(32, sizeof(char));
+    // shift the array so it has the same number
+    // of rows as 'n' - shifted values is replaced
+    // with <NA>
+    // see shift.h for more details
+    shift_array(offset_real, n, start_idx);
 
-    // clang-format off
-    snprintf(
-      name_buffer, 
-      32, 
-      "%s%d", 
-      maTypeName, 
-      optInTimePeriod
-    );
-    // clang-format on
-
-    colname[j + 1] = name_buffer;
-
-    UNPROTECT(protect_inner);
+    char *character_buffer = (char *)R_alloc(32, sizeof(char));
+    snprintf(character_buffer, 32, "%s%d", _MAType_(ma_type), period);
+    colname[j + 1] = character_buffer;
   }
 
-  // set column names
+  // set the remaining column names
+  // see names.h for more details
   column_names(output, n_cols, colname);
 
   UNPROTECT(protection_count);
