105692a0014c7afe2614f0d9669b5badef1f0b92
serkor1 <77464572+serkor1@users.noreply.github.com>
2026-07-26 14:53:31 +0200

    :books: Add periods column to template generation
    * All output is documented via roxygen2 templates
      which fails for MAVP due to missing periods column
      + The addition here avoids messing with the data-raw
        files. And documentation is generally generated once
        so its basically zero cost.

--- man-roxygen/returns.R
+++ man-roxygen/returns.R
@@ -2,4 +2,4 @@
 #'
 #' An object of same [class] and [length] of `x`:
 #'
-#' `r generate_returns_section(<%= tolower(.fun) %>(talib::BTC))`
+#' `r BTC <- talib::BTC; BTC$periods <- runif(nrow(BTC), 10, 20); generate_returns_section(<%= tolower(.fun) %>(BTC))`
