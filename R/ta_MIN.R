c83060246c2bcb7d771b164fb101a3b022050df2
serkor1 <77464572+serkor1@users.noreply.github.com>
2026-07-27 09:27:47 +0200

    :hammer: Enable (rolling) math operators
    * Re-introduced rolling minimum, maximum and sum.
      + The naming has been changed to accomodate the more
        verbose naming.

---
+++ R/ta_MIN.R
@@ -0,0 +1,83 @@
+#' @export
+#' @family Math Operators
+#'
+#' @title Lowest value over a specified period
+#' @templateVar .title Lowest value over a specified period
+#' @templateVar .author Serkan Korkmaz
+#' @templateVar .fun rolling_mininimum
+#'
+## splice:documentation:start
+## splice:documentation:end
+#'
+#' @template rolling_description
+#'
+#' @template rolling_returns
+rolling_mininimum <- function(
+	x,
+	timePeriod = 30,
+	na.bridge = FALSE
+) {
+	UseMethod("rolling_mininimum")
+}
+
+#' @export
+#' @usage NULL
+#' @rdname rolling_mininimum
+#'
+#' @aliases rolling_mininimum
+MIN <- rolling_mininimum
+
+#' @usage NULL
+#' @aliases rolling_mininimum
+#'
+#' @export
+rolling_mininimum.default <- function(
+	x,
+	timePeriod = 30,
+	na.bridge = FALSE
+) {
+	## calculate indicator and
+	## return as data.frame
+	x <- .Call(
+		C_impl_ta_MIN,
+		## splice:call:start
+		as.double(x),
+		as.integer(timePeriod),
+		## splice:call:end
+		as.logical(na.bridge)
+	)
+
+	## strip dimensions
+	## while preserving
+	## attributes
+	dim(x) <- NULL
+
+	## return indicator
+	x
+}
+
+#' @usage NULL
+#' @aliases rolling_mininimum
+#'
+#' @export
+rolling_mininimum.numeric <- function(
+	x,
+	timePeriod = 30,
+	na.bridge = FALSE
+) {
+	## calculate indicator and
+	## return as data.frame
+	x <- rolling_mininimum.default(
+		x = x,
+		timePeriod = timePeriod,
+		na.bridge = na.bridge
+	)
+
+	## strip dimensions
+	## while preserving
+	## attributes
+	dim(x) <- NULL
+
+	## return indicator
+	x
+}
---
+++ R/ta_SUM.R
@@ -0,0 +1,83 @@
+#' @export
+#' @family Math Operators
+#'
+#' @title Summation
+#' @templateVar .title Summation
+#' @templateVar .author Serkan Korkmaz
+#' @templateVar .fun rolling_sum
+#'
+## splice:documentation:start
+## splice:documentation:end
+#'
+#' @template rolling_description
+#'
+#' @template rolling_returns
+rolling_sum <- function(
+	x,
+	timePeriod = 30,
+	na.bridge = FALSE
+) {
+	UseMethod("rolling_sum")
+}
+
+#' @export
+#' @usage NULL
+#' @rdname rolling_sum
+#'
+#' @aliases rolling_sum
+SUM <- rolling_sum
+
+#' @usage NULL
+#' @aliases rolling_sum
+#'
+#' @export
+rolling_sum.default <- function(
+	x,
+	timePeriod = 30,
+	na.bridge = FALSE
+) {
+	## calculate indicator and
+	## return as data.frame
+	x <- .Call(
+		C_impl_ta_SUM,
+		## splice:call:start
+		as.double(x),
+		as.integer(timePeriod),
+		## splice:call:end
+		as.logical(na.bridge)
+	)
+
+	## strip dimensions
+	## while preserving
+	## attributes
+	dim(x) <- NULL
+
+	## return indicator
+	x
+}
+
+#' @usage NULL
+#' @aliases rolling_sum
+#'
+#' @export
+rolling_sum.numeric <- function(
+	x,
+	timePeriod = 30,
+	na.bridge = FALSE
+) {
+	## calculate indicator and
+	## return as data.frame
+	x <- rolling_sum.default(
+		x = x,
+		timePeriod = timePeriod,
+		na.bridge = na.bridge
+	)
+
+	## strip dimensions
+	## while preserving
+	## attributes
+	dim(x) <- NULL
+
+	## return indicator
+	x
+}
