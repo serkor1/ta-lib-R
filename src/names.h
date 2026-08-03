7a2b3f54c501170da320bad396d2f5820ea3c7b2
serkor1 <77464572+serkor1@users.noreply.github.com>
2026-08-03 07:41:09 +0200

    :hammer: Introduce index() and set_index()
    * The index refers to the time-index of the object
      passed into the indicator functions.
      By default, for <matrix> and <data.frame>, it (was) is assumed
      that these are given in the rownames() of the passed OHLC-V
      object. For an <xts>-object the time-index is given as the index attribute - that is just
      how <xts> works which necessitates this change.
    
    * With this introduction <xts>, <data.frame> and <matrix> objects can be handled
      in the default method which reduces the number of lines changed for the introduction
      of the <xts>-methods. Any changes and bugs can potentially be handled in the dispatching functions
      like, for example, series.xts() to avoid a mass change in files for a simple bug.

--- src/names.h
+++ src/names.h
@@ -4,7 +4,8 @@
 #include <Rinternals.h>
 
 void set_colnames(SEXP x, const char *const *names, int k);
-void rownames_data_frame(SEXP x, SEXP rownames);
-void rownames_matrix(SEXP x, SEXP rownames, SEXP colnames);
+void index_data_frame(SEXP x, SEXP rownames);
+void index_matrix(SEXP x, SEXP rownames, SEXP colnames);
+void index_xts(SEXP x, SEXP index);
 
 #endif /* NAMES_H */
